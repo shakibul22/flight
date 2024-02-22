816f6e2fa63f95ff2166921afd33bd0b187342a5
shakibul22 <shakibulislam684@gmail.com>
2024-02-22 14:49:10 +0600

    context use

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {  RouterProvider,} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./Routes";
 
+import Context from "./Context/Context";
 
+// const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <Context>
     <RouterProvider router={router} />
+    </Context>
+    
+    {/* </Provider> */}
   </React.StrictMode>
 );
